49f98afe5893c00bf754344843594cff415fce8c
Corey Oliphant <corey.oliphant@icloud.com>
2025-10-06 10:16:59 -0500

    Initial Commit

---
+++ CryptKeeper/CryptKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptKeeperApp.swift
+//  CryptKeeper
+//
+//  Created by Oliphant, Corey Dean on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptKeeperTests/CryptKeeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptKeeperTests.swift
+//  CryptKeeperTests
+//
+//  Created by Oliphant, Corey Dean on 10/6/25.
+//
+
+import Testing
+@testable import CryptKeeper
+
+struct CryptKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
